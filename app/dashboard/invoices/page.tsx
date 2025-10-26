b3a5cff451dd5cb5ca99b28a70104d7be868328c
Anhelina Rabtsevych <anhelina@ondaytrip.com>
2025-10-26 14:53:16 +0100

    project initial setup

---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,9 @@
+import { lusitana } from "@/app/ui/fonts";
+
+export default function Page() {
+  return (
+    <main className="flex min-h-screen flex-col p-6">
+      <p className={`${lusitana.className}`}>This is the invoices page</p>
+    </main>
+  );
+}
